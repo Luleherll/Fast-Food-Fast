0e19538401b8ee5983b288d840b0221935d2e6ab
lule herman <luleherll@gmail.com>
2018-10-09 23:23:44 +0300

    Change heroku bd connection

--- API/db.py
+++ API/db.py
@@ -8,8 +8,7 @@ class Database:
             conn = pg.connect(dbname='fasttests', user='postgres',
                               password='0789')
         else:
-            conn = pg.connect(dbname='d88tp0iv0aqbgc', user='dmyozauakukgim',
-                              password='78c6acc65cef8bc118a1e044dd422393a21a1018ee9cabbcd9769b4451ae6032')
+            conn = pg.connect('postgres://dmyozauakukgim:78c6acc65cef8bc118a1e044dd422393a21a1018ee9cabbcd9769b4451ae6032@ec2-54-225-68-133.compute-1.amazonaws.com:5432/d88tp0iv0aqbgc')
         self.conn = conn
         self.conn.autocommit = True
         self.cur = conn.cursor()
