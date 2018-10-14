480500a6f8be937185099f7a437b5bcf759b290b
lule herman <luleherll@gmail.com>
2018-10-14 13:59:29 +0300

    Set up challenge 4 files

---
+++ APP/routes.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found_error(e):
+    return "<h1>You're lost in the woods:<br> Go back to index:<h1>\
+<a href='https://lule-persistent.herokuapp.com/'>Click Here<a>", 404
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from API.models import Users, Orders, Menu
-from API.db import Database
-from API.routes import app
-from API.validation import Check
+from APP.routes import app
 
 if __name__ == '__main__':
     app.run(debug=True)
