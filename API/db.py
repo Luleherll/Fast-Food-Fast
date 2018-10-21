eb919d9890171cd9332789b0370f790ca6e67425
lule herman <luleherll@gmail.com>
2018-10-21 14:59:00 +0300

    Remove unwanted strings

--- API/db.py
+++ API/db.py
@@ -3,10 +3,6 @@ from psycopg2.extras import RealDictCursor
 
 
 class Database:
-    """dbname='dd9mb4gqm802l1' user='envcqlrpkysjxa'\
-        host='ec2-23-21-147-71.compute-1.amazonaws.com'\
-    port='5432'\
-    password='428e7756143a868108cd0392055a879b31faae3e92ca3969ea26f14b9709b566'"""
 
     def __init__(self, app):
         if app.config['TESTING'] is True:
