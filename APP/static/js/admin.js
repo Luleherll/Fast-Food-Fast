955835181271a19aac750a08e7b402896b4c4858
lule herman <luleherll@gmail.com>
2018-10-24 20:14:20 +0300

    Implement updating food, deleting food [Delivers #161345412] [Delivers #161345496]

--- APP/static/js/admin.js
+++ APP/static/js/admin.js
@@ -3,6 +3,7 @@ var pendingOrders = document.getElementsByClassName('slides')[2];
 var archive = document.getElementsByClassName('slides')[0];
 var updateSelect = document.getElementsByTagName('select')[0];
 var deleteSelect = document.getElementsByTagName('select')[1];
+
 fetch('https://lule-persistent.herokuapp.com/api/v2/orders/', {
 			method: 'get',
 			mode: 'cors',
@@ -217,20 +218,30 @@ fetch('https://lule-persistent.herokuapp.com/api/v2/menu', {
        name2.innerText=food.name
        updateSelect.appendChild(name1)
        deleteSelect.appendChild(name2)
-       if(updateSelect.options[updateSelect.selectedIndex].text==food.name){
+       
+       updateSelect.addEventListener('change', function(){
+         if(updateSelect.options[updateSelect.selectedIndex].text==food.name){
            document.getElementById('uPrice').value = food.price
            document.getElementById('uStatus').value = food.status
            document.getElementById('uTags').value = food.tags
        }
-       
+      });
+       deleteSelect.addEventListener('change', function(){
+    if(deleteSelect.options[deleteSelect.selectedIndex].text==food.name){
+      document.getElementById('dPrice').value = food.price
+      document.getElementById('dStatus').value = food.status
+      document.getElementById('dTags').value = food.tags
+  }
+});
     })
 })
 
 document.getElementById('close').addEventListener("click", function(){
 	close('banner')
 });
+
 document.getElementById('aFood').addEventListener('click', function() {
-    document.getElementById('yes').style.display='none';
+
     fetch('https://lule-persistent.herokuapp.com/api/v2/menu', {
     method: 'post',
     mode: 'cors',
@@ -271,4 +282,93 @@ document.getElementById('aFood').addEventListener('click', function() {
         }
     }
 })})
- 
+
+document.getElementById('show1').addEventListener("click", function(){
+	document.getElementsByClassName('confirms')[0].style.display='block'
+});
+document.getElementById('show2').addEventListener("click", function(){
+	document.getElementsByClassName('confirms')[1].style.display='block'
+});
+document.getElementById('uFood').addEventListener('click', function() {
+    document.getElementsByClassName('confirms')[0].style.display='none';
+    fetch('https://lule-persistent.herokuapp.com/api/v2/menu', {
+    method: 'put',
+    mode: 'cors',
+	headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+sessionStorage.getItem('token')
+	}),
+	body: JSON.stringify({
+        "name": updateSelect.options[updateSelect.selectedIndex].text,
+        "price": document.getElementById('uPrice').value,
+        "status": document.getElementById('uStatus').value,
+        "tags": document.getElementById('uTags').value
+	})
+}).then(function(response) {
+    console.log(response.status)
+    sessionStorage.setItem('status', response.status)
+    return response.json();
+}).then(function(res) {
+    
+    var status=sessionStorage.getItem('status')
+    sessionStorage.removeItem('status')
+    console.log(res.msg)
+    
+	if(status==205){
+		document.getElementById('info').innerText=res.msg
+	    document.getElementById('myModal').style.display='block';
+	}else if(status==406){
+        document.getElementById('msg').innerText=res.msg
+        document.getElementById('banner').style.background='rgb(175, 9, 9)';
+        document.getElementById('banner').style.display='block';
+    }else{
+        if(res.msg!==undefined){
+            document.getElementById('msg').innerText=res.msg
+            document.getElementById('banner').style.background='rgba(90, 41, 21, 0.781)';
+            document.getElementById('banner').style.display='block';
+        }else{
+            document.getElementById('msg').innerText=res.error
+            document.getElementById('banner').style.display='block';
+        }
+    }
+})})
+document.getElementById('dFood').addEventListener('click', function() {
+    document.getElementsByClassName('confirms')[1].style.display='none';
+    fetch('https://lule-persistent.herokuapp.com/api/v2/menu', {
+    method: 'delete',
+    mode: 'cors',
+	headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+sessionStorage.getItem('token')
+	}),
+	body: JSON.stringify({
+        "name": updateSelect.options[updateSelect.selectedIndex].text,
+	})
+}).then(function(response) {
+    console.log(response.status)
+    sessionStorage.setItem('status', response.status)
+    return response.json();
+}).then(function(res) {
+    
+    var status=sessionStorage.getItem('status')
+    sessionStorage.removeItem('status')
+    console.log(res.msg)
+    
+	if(status==200){
+		document.getElementById('info').innerText=res.msg
+	    document.getElementById('myModal').style.display='block';
+	}else if(status==406){
+        document.getElementById('msg').innerText=res.msg
+        document.getElementById('banner').style.background='rgb(175, 9, 9)';
+        document.getElementById('banner').style.display='block';
+    }else{
+        if(res.msg!==undefined){
+            document.getElementById('msg').innerText=res.msg
+            document.getElementById('banner').style.background='rgba(90, 41, 21, 0.781)';
+            document.getElementById('banner').style.display='block';
+        }else{
+            document.getElementById('msg').innerText=res.error
+            document.getElementById('banner').style.display='block';
+        }
+    }
+})})
